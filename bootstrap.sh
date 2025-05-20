4bf1e4d3b3996303d103db0ad5d5d7b6f5bc88d5
Daniel Gaitán <dgaitan@calicofox.dev>
2025-05-20 14:16:21 -0500

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#Stow dotfiles
+git clone https://github.com/ParzivalVulpine/dotfiles
+cd dotfiles/
+./stow-this.sh
+
+pacman -S rustup
+rustup default stable
+
+#Install paru
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+
+#Setup Mirrors
+paru -S shiny-mirrors
+
+sudo shiny-mirrors config -c Global, Argentina, Brazil, Colombia, Ecuador, Peru, United States -c South America -p Https, Rsync
+
+sudo shiny-mirrors refresh
+
+
+#Download packages
+#CLI stuff
+paru -S base base-devel fish slack-desktop github-cli btop man-db neovim xdg-utils xdg-ninja xdg-desktop-portal-hyprland starship thefuck tmux fastfetch stow lazygit tldr zoxide fortune
+
+#ags-hyprpanel-git
+sudo pacman -S --needed wireplumber libgtop bluez bluez-utils btop dart-sass wl-clipboard brightnessctl swww python upower pacman-contrib power-profiles-daemon gvfs wf-recorder
+yay -S --needed aylurs-gtk-shell-git grimblast-git hyprpicker matugen-bin hyprsunset-git
+
+#Utils
+paru -S hypridle hyprlock ags-hyprpanel-git hyprpaper hyprpicker hyprpolkitagent hyprsysteminfo rofi-wayland ttf-space-mono-nerd
+
+#Desktop Apps
+paru -S ghostty google-chrome slack-desktop spotify discord godot thunderbird telegram steam
+
+#Neovim deps
+paru -S fzf curl ripgrep fd tree-sitter-cli
